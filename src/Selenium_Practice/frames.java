6af2d31b364fdd472eb36be24d7a0fa4c8dd89e4
jasmeenbajwa <jasmeen1997kaur16@gmail.com>
2021-09-05 23:52:32 -0400

    Nested Frames done

--- src/Selenium_Practice/frames.java
+++ src/Selenium_Practice/frames.java
@@ -13,7 +13,8 @@ public class frames {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		frame_Selenium();
+		//frame_Selenium();
+		NestedFrames();
 		
 		
 		
@@ -48,5 +49,32 @@ public class frames {
 		
 	}
 	
+	
+	public static void NestedFrames()
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\jasme\\\\Downloads\\\\chromedriver_win32 (1)//chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		JavascriptExecutor js= (JavascriptExecutor) driver;
+		driver.get("https://www.demoqa.com/");
+		WebElement card= driver.findElement(By.xpath("//div[@class='home-body']"));
+		card.click();
+		driver.findElement(By.cssSelector("a[id='close-fixedban']")).click();
+		WebElement frameTab= driver.findElement(By.xpath("//div[@class='accordion']/div[3]"));
+		js.executeScript("arguments[0].scrollIntoView();", frameTab);
+		driver.findElement(By.xpath("//div[@class='accordion']/div[3]/div/ul/li[4]")).click();
+		driver.switchTo().frame("frame1");
+		System.out.println(driver.findElement(By.tagName("body")).getText()); //parent frame
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@srcdoc='<p>Child Iframe</p>']")));
+		System.out.println(driver.findElement(By.tagName("p")).getText());// child frame
+		driver.switchTo().defaultContent();
+		System.out.println(driver.findElement(By.tagName("body")).getText());// back to main frame
+		
+		//  Nested Frames done
+	}
+	
+	}
 
-}
+
